aa004d256b0dceb28ca90096a7aa390afe481c3c
bed-com-07-20 <bed-com-07-20@unima.ac.mw>
2023-06-09 16:32:26 +0200

    online hosting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,29 @@
-
-/*import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-
-@Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}*/
-
-
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { user } from './user.entity';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([user]),
+  imports: [
+    TypeOrmModule.forFeature([user]),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: 'http://sql9.freesqldatabase.com',
       port: 3306,
-      username: '',
-      password: '',
-      database: 'uomo_api',
+      username: 'sql9624879',
+      password: 'sQsMUyeSUv',
+      database: 'sql9624879',
       entities: [user],
-      synchronize: true,
+      synchronize: false,
     }),
     TypeOrmModule.forFeature([user]),
     JwtModule.register({
       secret: 'secret',
-      signOptions: {expiresIn: '1d'}
-    })
-   // UsersModule
+      signOptions: { expiresIn: '1d' },
+    }),
+    // UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
